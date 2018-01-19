3f886453e41c2bae0b73238700dd050e5ac27ea3
CMHao <menghao88@hotmail.com>
2018-01-19 12:47:42 +0800

    upload
    upload

--- GE_Assignment/Assets/DoorScript.cs
+++ GE_Assignment/Assets/DoorScript.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour {
 
+    private Animator _animator;
+
 	// Use this for initialization
 	void Start () {
-		
+        _animator = GetComponent<Animator>();
 	}
-	
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _animator.SetBool("open", true);
+
+        }
+    }
 	// Update is called once per frame
 	void Update () {
 		

dee5a9c1567cc4f529133ed6178b9bedb685e03d
MayGeeChua <33078670+MayGeeChua@users.noreply.github.com>
2018-01-22 19:34:51 +0800

    Environment
    Texture is added

---
+++ GE_Assignment/Assets/FlickerLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerLight : MonoBehaviour {
+
+    Light lampLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+	// Use this for initialization
+	void Start () {
+        lampLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+	}
+	
+	IEnumerator Flashing ()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+            lampLight.enabled = !lampLight.enabled;
+        }
+    }
+}

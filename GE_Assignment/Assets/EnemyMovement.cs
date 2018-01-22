c352c498cddcd44278f2ed7f76ff538902d3633e
CMHao <menghao88@hotmail.com>
2018-01-22 12:50:53 +0800

    AI go
    implement sound first

---
+++ GE_Assignment/Assets/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    Animator anim;
+    int speed;
+    UnityEngine.AI.NavMeshAgent nav;
+
+    // Use this for initialization
+
+    public void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+
+    public void Pause()
+    {
+        anim.enabled = false;
+        nav.enabled = false;
+    }
+    public void Restart()
+    {
+        anim.enabled = true;
+        nav.enabled = true;
+    }
+}
---
+++ GE_Assignment/Assets/FlashlightHit.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightHit : MonoBehaviour {
+
+    public float range = 500f;
+
+    Ray shootRay;
+    int shootable;
+    RaycastHit shootHit;
+    LineRenderer LightLine;
+    float timer;
+    float hittimer;
+    float timebetween = 0.15f;
+    float effectsDisplayTime = 0.2f;
+
+
+    // Use this for initialization
+    void Start () {
+        LightLine = GetComponent<LineRenderer>();
+        shootable = LayerMask.GetMask("shootable");
+        hittimer = 0f;
+        timer = 0f;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetButton("Fire1"))
+        {
+            Stop();
+        }
+        if (hittimer >= timebetween *effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+        if (timer >= 1.0f)
+        {
+            if (Physics.Raycast(shootRay, out shootHit, range, shootable))
+            {
+                EnemyMovement Enemymovement = shootHit.collider.GetComponent<EnemyMovement>();
+                Enemymovement.Start();
+                Enemymovement.Update();
+            }
+        }
+    }
+
+    void Stop()
+    {
+        timer = 0f;
+        LightLine.enabled = true;
+        LightLine.SetPosition(0, transform.position);
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+        if (Physics.Raycast (shootRay, out shootHit, range, shootable))
+        {
+            EnemyMovement Enemymovement = shootHit.collider.GetComponent<EnemyMovement>();
+            if (Enemymovement != null)
+            {
+                timer += Time.deltaTime;
+                Enemymovement.Pause();
+            }
+            LightLine.SetPosition(1, shootHit.point);
+        }
+        else
+        {
+            LightLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+
+    public void DisableEffects()
+    {
+        LightLine.enabled = false;
+    }
+}

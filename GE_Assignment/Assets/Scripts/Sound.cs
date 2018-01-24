70b268f19c8d27170ca7ae41ffed97c08649def6
Ciellee <33077072+Ciellee@users.noreply.github.com>
2018-01-24 17:48:35 +0800

    Add bgm
    just bgm
    -enemy havent have sound

---
+++ GE_Assignment/Assets/Scripts/Sound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+
+	public AudioSource SoundSource;
+	public AudioClip Sounds;
+
+
+	void OnTriggerEnter(Collider Other)
+	{
+		if (Other.tag == "Player" )
+			{
+			SoundSource.Play ();
+			}
+	}
+		
+}
